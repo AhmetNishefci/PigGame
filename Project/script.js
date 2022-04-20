62d3117ec33421d404e92e4a598cdb437760448d
AhmetNishefci <metti.ni@hotmail.com>
2022-04-20 16:57:27 +0200

    Switching players

--- Project/script.js
+++ Project/script.js
@@ -9,8 +9,11 @@ const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 const current0 = document.querySelector("#current--0");
 const current1 = document.querySelector("#current--1");
+const player1 = document.querySelector(".player--0");
+const player2 = document.querySelector(".player--1");
 
 let currentScore = 0;
+let activePlayer = 0;
 
 // Starting conditions
 score0.textContent = 0;
@@ -29,7 +32,13 @@ btnRoll.addEventListener("click", () => {
   if (random !== 1) {
     //Add dice to current score
     currentScore += random;
-    current0.textContent = currentScore;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player1.classList.toggle("player--active");
+    player2.classList.toggle("player--active");
   }
 });
